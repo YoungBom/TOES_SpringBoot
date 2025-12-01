7ef0c6ea19b117d83c44d1d3137884df90266d2d
YoungBom <p01039974051@gmail.com>
2025-12-01 09:16:33 +0900

    up
    up

---
+++ target/classes/static/js/cart/cartList.js
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+
+//전체 체크박스를 클릭했을 때
+const checkAll = document.getElementById("checkAll");
+const ch = document.querySelectorAll(".ch")
+const frm = document.getElementById("frm")
+const del = document.getElementById("del")
+const add = document.getElementById("add")
+
+add.addEventListener("click", ()=>{
+	//체크 된것들이 하나 이상인지 검증
+	frm.setAttribute("action", "/account/add")
+	frm.submit();
+})
+
+del.addEventListener("click", ()=>{
+	//체크 된것들이 하나 이상인지 검증
+	frm.submit();
+})
+
+checkAll.addEventListener("click", ()=>{
+	
+	ch.forEach((item)=>{
+		console.log( checkAll.checked)
+		item.checked=checkAll.checked
+	});
+	
+})
+
+ch.forEach((item)=>{
+	item.addEventListener("click", ()=>{
+		let flag=true;
+		ch.forEach((c)=>{
+			console.log(c.checked)
+			if(!c.checked){
+				flag=false;
+				return;
+			}
+		})
+		checkAll.checked=flag;
+	})
+})
