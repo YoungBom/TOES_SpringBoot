4c24c9cda0d5ba54c2fa9931028452e30032a92d
YoungBom <p01039974051@gmail.com>
2025-12-09 14:06:56 +0900

    up
    up

--- src/main/java/com/winter/app/config/security/SecurityConfig.java
+++ src/main/java/com/winter/app/config/security/SecurityConfig.java
@@ -52,6 +52,8 @@ public class SecurityConfig {
 				form
 					//로그인폼 jsp 경로로 가는 url과 로그인 처리 url 작성
 					.loginPage("/users/login")
+					//로그인 진행할 URL
+					.loginProcessingUrl("/users/login")
 					//.usernameParameter("id")
 					//.passwordParameter("pw")
 					.defaultSuccessUrl("/")
