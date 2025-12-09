4cb8dd1f5698c6375c29c4f40638aa6e759b78cc
YoungBom <p01039974051@gmail.com>
2025-12-09 14:04:05 +0900

    up
    up

--- src/main/java/com/winter/app/home/HomeController.java
+++ src/main/java/com/winter/app/home/HomeController.java
@@ -1,17 +1,45 @@
 package com.winter.app.home;
 
+import java.security.Principal;
+import java.util.Enumeration;
+
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.winter.app.users.UserDTO;
+
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 public class HomeController {
 
+    private final HomeController HomeController;
+
+    HomeController(HomeController homeController) {
+        this.HomeController = homeController;
+    }
+
 	@GetMapping("/")
-	public String index()throws Exception{
-		log.info("index contorller");
-		return "index";
+	public String index(HttpSession session)throws Exception{
+		// 1.
+		Enumeration<String> en = session.getAttributeNames();
+		
+		while(en.hasMoreElements()) {
+			String k = en.nextElement();
+			System.out.println(k);
+			return "index";
+		}
+	
+	public String index4(Principal principal)throws Exception{
+		UserDTO userDTO = (UserDTO)principal;
+		System.out.println(userDTO.getUsername());
+		return "index4";
+		
+	
+		
+		
 	}
 }
